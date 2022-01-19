7b26d6ff817969c6b01b032bd89b8cba8e255526
Mgs. Tabrani <63233752+mgstabrani@users.noreply.github.com>
2022-01-19 16:22:03 +0700

    feature: education data (#5)

---
+++ data/education.js
@@ -0,0 +1,14 @@
+const education = [
+    {
+        institution: 'SMAN Sumatera Selatan',
+        major: 'Science',
+        period: 'July 2016 - May 2019',
+        website: 'https://www.itb.ac.id/',
+    },
+    {
+        institution: 'Institut Teknologi Bandung',
+        major: 'Informatics',
+        period: 'August 2019 - present',
+        website: 'http://smansumsel.sch.id/',
+    },
+]
