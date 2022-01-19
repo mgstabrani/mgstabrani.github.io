a3953f4a4a62b0be7def7d971e8408b8e81b5359
Mgs. Tabrani <63233752+mgstabrani@users.noreply.github.com>
2022-01-19 17:57:16 +0700

    feature-fix: education website (#7)

--- data/education.js
+++ data/education.js
@@ -3,12 +3,12 @@ const education = [
         institution: 'SMAN Sumatera Selatan',
         major: 'Science',
         period: 'July 2016 - May 2019',
-        website: 'https://www.itb.ac.id/',
+        website: 'http://smansumsel.sch.id/',
     },
     {
         institution: 'Institut Teknologi Bandung',
         major: 'Informatics',
         period: 'August 2019 - present',
-        website: 'http://smansumsel.sch.id/',
+        website: 'https://www.itb.ac.id/',
     },
 ]
