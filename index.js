0805b49f9bb74f1382f5d1582544584a15fa8113
Mgs. Tabrani <63233752+mgstabrani@users.noreply.github.com>
2022-01-30 10:52:55 +0700

    feature: alert (#13)

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const DEV_MODE = true;
 const body = document.body;
 
-if(! DEV_MODE) document.getElementById("development").hidden = true;
+if(! DEV_MODE) { 
+    document.getElementById("development").hidden = true;
+    alert("Don't open it! You'll find that I'm not good enough at UI design and front end :)")
+}
 else document.getElementById("production").hidden = true;
