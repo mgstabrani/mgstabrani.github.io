68526cf9cfb210812dcae951b3f330e1e2797b7e
mgstabrani <13519122@std.stei.itb.ac.id>
2022-01-30 10:40:29 +0700

    feature: summary info

---
+++ data/social_media.js
@@ -0,0 +1,27 @@
+const socialMedia = [
+    {
+        name: 'Github',
+        url: 'https://github.com/mgstabrani',
+        image: 'https://img.shields.io/badge/github-000000?style=for-the-badge&logo=github&logoColor=white',
+    },
+    {
+        name: 'Gitlab',
+        url: 'https://gitlab.com/mgstabrani',
+        image: 'https://img.shields.io/badge/GitLab-330F63?style=for-the-badge&logo=gitlab&logoColor=white',
+    },
+    {
+        name: 'Linkedin',
+        url: 'https://linkedin.com/in/mgs-tabrani/',
+        image: 'https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white',
+    },
+    {
+        name: 'Instagram',
+        url: 'https://instagram.com/mgs_tabrani',
+        image: 'https://img.shields.io/badge/instagram-%23E4405F.svg?&style=for-the-badge&logo=instagram&logoColor=white',
+    },
+    {
+        name: 'Youtube',
+        url: 'https://youtube.com/channel/UCbWnNIz0i_ebDGHbhQhFqRQ',
+        image: 'https://img.shields.io/badge/YouTube-FF0000?style=for-the-badge&logo=youtube&logoColor=white',
+    },
+];
---
+++ data/summary.js
@@ -0,0 +1,1 @@
+const summary = 'I am a third year Informactics student at Institut Teknologi Bandung. I have more than 1 year of experience in software engineering by joining internships. I have several experiences in building monolithic web services and microservices during my internships.';
