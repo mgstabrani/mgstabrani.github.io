60cce82f9b09e48b7e31aa57cf4b178f4471b62f
mgstabrani <13519122@std.stei.itb.ac.id>
2022-04-05 17:12:15 +0700

    feature-fix: delete pop up alert

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ const body = document.body;
 
 if(! DEV_MODE) { 
     document.getElementById("development").hidden = true;
-    alert("Don't open it! You'll find that I'm not good enough at UI design and front end :)")
 }
 else document.getElementById("production").hidden = true;
