8d57c4fd6e9eb03f488c7a2e60f35de6b227e196
Mgs. Tabrani <mgstabrani19@gmail.com>
2022-08-01 13:53:30 +0700

    data: gcp stack (#27)
    * data: gcp stack

--- data/tech_stack.js
+++ data/tech_stack.js
@@ -64,4 +64,9 @@ const techStack = [
         url: 'https://graphql.org/',
         image: 'https://github.com/get-icon/geticon/raw/master/icons/graphql.svg'
     },
+    {
+        name: 'Google Cloud Platform',
+        url: 'https://cloud.google.com/',
+        image: 'https://github.com/get-icon/geticon/raw/master/icons/google-cloud-platform.svg'
+    }
 ];
